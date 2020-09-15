e2e6579b3352c4a2aaac7414baabc86817d31622
ws0819 <57986480+ws0819@users.noreply.github.com>
2020-09-15 21:20:54 +0900

    Update 3.py

--- 3.py
+++ 3.py
@@ -18,7 +18,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 import tensorflow as tf
-#이미지가 저장될 폴더가 없다면 만듭니다.
+
 import os
 if not os.path.exists("./gan_images"):
     os.makedirs("./gan_images")
@@ -39,7 +39,7 @@ if gpus:
 np.random.seed(3)
 tf.random.set_seed(3)
 
-#생성자 모델을 만듭니다.
+
 generator = Sequential()
 generator.add(Dense(128*8*8, input_dim=100, activation=LeakyReLU(0.2))) #7*7은 어차피 뒤에서 2배씩 뻥튀기 하는거니까 그런거
 generator.add(BatchNormalization())
@@ -51,7 +51,7 @@ generator.add(Activation(LeakyReLU(0.2)))
 generator.add(UpSampling2D())
 generator.add(Conv2D(1, kernel_size=5, padding='same', activation='tanh'))
 
-#판별자 모델을 만듭니다.
+
 discriminator = Sequential()
 discriminator.add(Conv2D(64, kernel_size=5, strides=2, input_shape=(32,32,1), padding="same"))
 discriminator.add(Activation(LeakyReLU(0.2)))
@@ -64,7 +64,7 @@ discriminator.add(Dense(1, activation='sigmoid'))
 discriminator.compile(loss='binary_crossentropy', optimizer='adam')
 discriminator.trainable = False
 
-#생성자와 판별자 모델을 연결시키는 gan 모델을 만듭니다.
+
 ginput = Input(shape=(100,))
 dis_output = discriminator(generator(ginput))
 gan = Model(ginput, dis_output)
@@ -74,7 +74,8 @@ gan.summary()
 Dataset = namedtuple('Dataset', ['X', 'y'])
 N_CLASSES = 43
 RESIZED_IMAGE = (32, 32)
-#이미지 불러오기 함수
+
+
 def to_tf_format(imgs):
     return np.stack([img[:, :, np.newaxis] for img in imgs], axis=0).astype(np.float32)
 
@@ -127,38 +128,31 @@ for mb in minibatcher(X_train, y_train, 10000, True):
 
 
 
-
-
-
-
-#신경망을 실행시키는 함수를 만듭니다.
 def gan_train(epoch, batch_size, saving_interval):
-  
-  
+ 
 
 
   true = np.ones((batch_size, 1))
   fake = np.zeros((batch_size, 1))
 
   for i in range(epoch):
-          # 실제 데이터를 판별자에 입력하는 부분입니다.
+          
           idx = np.random.randint(0, X_train.shape[0], batch_size)
           imgs = X_train[idx]
           d_loss_real = discriminator.train_on_batch(imgs, true)
 
-          #가상 이미지를 판별자에 입력하는 부분입니다.
+          
           noise = np.random.normal(0, 1, (batch_size, 100))
           gen_imgs = generator.predict(noise)
           d_loss_fake = discriminator.train_on_batch(gen_imgs, fake)
 
-          #판별자와 생성자의 오차를 계산합니다.
+          
           d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
           g_loss = gan.train_on_batch(noise, true)
 
           print('epoch:%d' % i, ' d_loss:%.4f' % d_loss, ' g_loss:%.4f' % g_loss)
 
-        # 이부분은 중간 과정을 이미지로 저장해 주는 부분입니다. 본 장의 주요 내용과 관련이 없어
-        # 소스코드만 첨부합니다. 만들어진 이미지들은 gan_images 폴더에 저장됩니다.
+        
           if i % saving_interval == 0:
               #r, c = 5, 5
               noise = np.random.normal(0, 1, (25, 100))
