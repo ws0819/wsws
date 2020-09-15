d86400a109bdfb5c2860349ec820db59208c1afe
ws0819 <57986480+ws0819@users.noreply.github.com>
2020-09-15 21:20:11 +0900

    Update 2-2.py

--- 2-2.py
+++ 2-2.py
@@ -30,8 +30,8 @@ if gpus:
 
 np.random.seed(101)
 Dataset = namedtuple('Dataset', ['X', 'y'])
-N_CLASSES = 43
-RESIZED_IMAGE = (32, 32)
+aa = 43
+bb = (32, 32)
 
 def to_tf_format(imgs):
     return np.stack([img[:, :, np.newaxis] for img in imgs], axis=0).astype(np.float32)
@@ -58,7 +58,7 @@ def read_dataset_ppm(rootpath, n_labels, resize_to):
   return Dataset(X = to_tf_format(images).astype(np.float32),
                  y = np.matrix(labels).astype(np.float32))
 
-dataset = read_dataset_ppm('GTSRB/Final_Training/Images', N_CLASSES, RESIZED_IMAGE)
+dataset = read_dataset_ppm('GTSRB/Final_Training/Images', aa, bb)
 
 from sklearn.model_selection import train_test_split
 idx_train, idx_test = train_test_split(range(dataset.X.shape[0]), test_size=0.25, random_state=101)
