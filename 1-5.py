7ae87b5a7df97c7af86822464fea5eb54c16f52d
ws0819 <57986480+ws0819@users.noreply.github.com>
2020-09-15 20:59:48 +0900

    Add files via upload

---
+++ 1-5.py
@@ -0,0 +1,178 @@
+#-*- coding: utf-8 -*-
+from tensorflow.keras.datasets import cifar10
+
+from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from tensorflow.keras.layers import BatchNormalization, Activation, LeakyReLU, UpSampling2D, Conv2D
+from tensorflow.keras.models import Sequential, Model
+
+from collections import namedtuple
+from skimage.color import rgb2lab
+from skimage.transform import resize
+from sklearn.model_selection import train_test_split
+import glob
+
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import tensorflow as tf
+#이미지가 저장될 폴더가 없다면 만듭니다.
+import os
+if not os.path.exists("./gan_images"):
+    os.makedirs("./gan_images")
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
+
+np.random.seed(3)
+tf.random.set_seed(3)
+
+#생성자 모델을 만듭니다.
+generator = Sequential()
+generator.add(Dense(128*8*8, input_dim=100, activation=LeakyReLU(0.2))) #7*7은 어차피 뒤에서 2배씩 뻥튀기 하는거니까 그런거
+generator.add(BatchNormalization())
+generator.add(Reshape((8, 8, 128)))
+generator.add(UpSampling2D())
+generator.add(Conv2D(64, kernel_size=5, padding='same'))
+generator.add(BatchNormalization())
+generator.add(Activation(LeakyReLU(0.2)))
+generator.add(UpSampling2D())
+generator.add(Conv2D(1, kernel_size=5, padding='same', activation='tanh'))
+
+#판별자 모델을 만듭니다.
+discriminator = Sequential()
+discriminator.add(Conv2D(64, kernel_size=5, strides=2, input_shape=(32,32,1), padding="same"))
+discriminator.add(Activation(LeakyReLU(0.2)))
+discriminator.add(Dropout(0.3))
+discriminator.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
+discriminator.add(Activation(LeakyReLU(0.2)))
+discriminator.add(Dropout(0.3))
+discriminator.add(Flatten())
+discriminator.add(Dense(1, activation='sigmoid'))
+discriminator.compile(loss='binary_crossentropy', optimizer='adam')
+discriminator.trainable = False
+
+#생성자와 판별자 모델을 연결시키는 gan 모델을 만듭니다.
+ginput = Input(shape=(100,))
+dis_output = discriminator(generator(ginput))
+gan = Model(ginput, dis_output)
+gan.compile(loss='binary_crossentropy', optimizer='adam')
+gan.summary()
+
+Dataset = namedtuple('Dataset', ['X', 'y'])
+N_CLASSES = 43
+RESIZED_IMAGE = (32, 32)
+#이미지 불러오기 함수
+def to_tf_format(imgs):
+    return np.stack([img[:, :, np.newaxis] for img in imgs], axis=0).astype(np.float32)
+
+def read_dataset_ppm(rootpath, n_labels, resize_to):
+  images = []
+  labels = []
+  
+  for c in range(n_labels):
+    full_path = rootpath + '/' + format(c, '05d') + '/'
+    for img_name in glob.glob(full_path + "*.ppm"):
+      
+      img = plt.imread(img_name).astype(np.float32)
+      img = rgb2lab((img-127.5)/127.5)[:,:,0]
+      if resize_to:
+        img = resize(img, resize_to, mode='reflect')
+      
+      label = np.zeros((n_labels, ), dtype=np.float32)
+      label[c] = 1.0
+      
+      images.append(img.astype(np.float32))
+      labels.append(label)
+
+  return Dataset(X = to_tf_format(images).astype(np.float32),
+                 y = np.matrix(labels).astype(np.float32))
+
+dataset = read_dataset_ppm('GTSRB/Final_Training/Images', N_CLASSES, RESIZED_IMAGE)
+
+idx_train, idx_test = train_test_split(range(dataset.X.shape[0]), test_size=0.25, random_state=101)
+X_train = dataset.X[idx_train, :, :, :]
+X_test = dataset.X[idx_test, :, :, :]
+y_train = dataset.y[idx_train, :]
+y_test = dataset.y[idx_test, :]
+
+def minibatcher(X, y, batch_size, shuffle):
+  assert X.shape[0] == y.shape[0]
+  n_samples = X.shape[0]
+  
+  if shuffle:
+    idx = np.random.permutation(n_samples)
+  else:
+    idx = list(range(n_samples))
+  
+  for k in range(int(np.ceil(n_samples/batch_size))):
+    from_idx = k*batch_size
+    to_idx = (k+1)*batch_size
+    yield X[idx[from_idx:to_idx], :, :, :], y[idx[from_idx:to_idx], :]
+
+for mb in minibatcher(X_train, y_train, 10000, True):
+    print(mb[0].shape, mb[1].shape)
+
+
+
+
+
+
+
+#신경망을 실행시키는 함수를 만듭니다.
+def gan_train(epoch, batch_size, saving_interval):
+  
+  
+
+
+  true = np.ones((batch_size, 1))
+  fake = np.zeros((batch_size, 1))
+
+  for i in range(epoch):
+          # 실제 데이터를 판별자에 입력하는 부분입니다.
+          idx = np.random.randint(0, X_train.shape[0], batch_size)
+          imgs = X_train[idx]
+          d_loss_real = discriminator.train_on_batch(imgs, true)
+
+          #가상 이미지를 판별자에 입력하는 부분입니다.
+          noise = np.random.normal(0, 1, (batch_size, 100))
+          gen_imgs = generator.predict(noise)
+          d_loss_fake = discriminator.train_on_batch(gen_imgs, fake)
+
+          #판별자와 생성자의 오차를 계산합니다.
+          d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+          g_loss = gan.train_on_batch(noise, true)
+
+          print('epoch:%d' % i, ' d_loss:%.4f' % d_loss, ' g_loss:%.4f' % g_loss)
+
+        # 이부분은 중간 과정을 이미지로 저장해 주는 부분입니다. 본 장의 주요 내용과 관련이 없어
+        # 소스코드만 첨부합니다. 만들어진 이미지들은 gan_images 폴더에 저장됩니다.
+          if i % saving_interval == 0:
+              #r, c = 5, 5
+              noise = np.random.normal(0, 1, (25, 100))
+              gen_imgs = generator.predict(noise)
+
+              # Rescale images 0 - 1
+              gen_imgs = 0.5 * gen_imgs + 0.5
+
+              fig, axs = plt.subplots(5, 5)
+              count = 0
+              for j in range(5):
+                  for k in range(5):
+                      axs[j, k].imshow(gen_imgs[count, :, :, 0], cmap='gray')
+                      axs[j, k].axis('off')
+                      count += 1
+              fig.savefig("gan_images/gan_mnist_%d.png" % i)
+
+gan_train(4001, 32, 200) 
